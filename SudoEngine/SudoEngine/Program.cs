9923ca3a1da5b37d9f1a4c6dcebf7ef5cec143dc
YohannDR <yohann.demaison-regne@orange.fr>
2021-07-10 14:21:49 +0200

    Ajout fichier release

--- SudoEngine/SudoEngine/Program.cs
+++ SudoEngine/SudoEngine/Program.cs
@@ -8,7 +8,7 @@ namespace SudoEngine
         {
             using (Window window = new Window(900, 600, "SudoEngine"))
             {
-                window.Run(60, 60);
+                window.Run(10, 60);
                 window.VSync = VSyncMode.On;
             }
         }
